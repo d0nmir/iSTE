a50a8be48d7f8d20cb7df6148838b743b2211174
Damir <d.abillkasimov01@gmail.com>
2025-11-16 20:16:52 +0500

    Fix artifact path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true, // слушать на всех интерфейсах
-    port: 5173, // порт для Live Share
-  },
+  build: {
+    outDir: 'build' // теперь Vite будет собирать в папку build
+  }
 })
