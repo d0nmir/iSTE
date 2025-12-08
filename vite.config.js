281902c1f31cc638a70c57fe88a8c99555cff5ce
Damir <d.abillkasimov01@gmail.com>
2025-12-08 06:48:53 +0500

    Prepare for deployment

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/<repo-name>/",
   plugins: [react()],
+  server: {
+    host: true, 
+    port: 5173  
+  },
   build: {
     outDir: 'build'
   }
-});
-
+})
